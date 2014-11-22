2a992997351e7c2371c10aa977c3f3769e703eec
deepak <sihag.deepak@gmail.com>
2014-11-22 12:34:37 +0530

    testing

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sihag.deepak@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures hadoop"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.12"
 
