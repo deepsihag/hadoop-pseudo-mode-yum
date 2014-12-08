8abdaaef838fca36136e71dd414a7064594e882f
Deepak Sihag <sihag.deepak@gmail.com>
2014-12-08 09:51:37 +0530

    test rebase

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures hadoop"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-
